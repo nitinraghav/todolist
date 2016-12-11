982a2e9c6b4d07057b2a5eeeb4d16ef6e4f03139
nitinraghav <nitinraghav@yahoo.com>
2016-12-11 02:46:35 -0500

    added user interface

--- todolist.js
+++ todolist.js
@@ -68,14 +68,33 @@ var todoList = {
 	}	
 };
 
-var displayTodosButton = document.getElementById("displayTodosButton");
-var toggleAllButton = document.getElementById("toggleAllButton");
-
-displayTodosButton.addEventListener("click", function(){
-	todoList.displayTodo();
-});
-
-
-toggleAllButton.addEventListener("click", function(){
-	todoList.toggleAll();
-});
+var handlers = {
+	displayTodo: function(){
+		todoList.displayTodo();
+	},
+	addTodo: function(){
+		var addTodoTextInput = document.getElementById("addTodoTextInput");
+		todoList.addTodo(addTodoTextInput.value);
+		addTodoTextInput.value = "";
+	},
+	changeTodo: function(){
+		var changeTodoPositionInput = document.getElementById("changeTodoPositionInput");
+		var changeTodoTextInput = document.getElementById("changeTodoTextInput");
+		todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+		changeTodoPositionInput.value = "";
+		changeTodoTextInput.value = "";
+	},
+	deleteTodo: function(){
+		var deleteTodoPositionInput = document.getElementById("deleteTodoPositionInput");
+		todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+		deleteTodoPositionInput.value = "";
+	},
+	toggleCompleted: function(){
+		var toggleCompletedPositionInput =document.getElementById("toggleCompletedPositionInput");
+		todoList.toggleTodo(toggleCompletedPositionInput.valueAsNumber);
+		toggleCompletedPositionInput.value = "";		
+	},
+	toggleAll: function(){
+		todoList.toggleAll();
+	}
+};
